44e3df748476f88b412ab36aedf894036b1e4180
admin <admin@DESKTOP-7E5APTE>
2022-01-26 20:53:22 +0530

    firsr comit byt user 1

---
+++ src/test/java/com/github/practice1/GitTest2.java
@@ -0,0 +1,15 @@
+package com.github.practice1;
+
+import org.testng.annotations.Test;
+
+public class GitTest2 {
+	@Test
+	public void gitTest1()
+	{
+		
+		System.out.println("user 1-  1st comit");
+	}
+
+
+
+}
